4947490804f5cf1ee3fb200edb51a48edfa28ef6
xiaotiangg <xiaotiangg2003@outlook.com>
2024-02-10 23:53:19 +0800

    导出运营报表

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -71,5 +72,15 @@ public class ReportController {
             @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end) {
         log.info("查询销量排名top10:{} {}", begin, end);
         return Result.success(reportService.getTop10(begin, end));
+    }
+
+    /**
+     * 导出运营数据报表
+     * @param httpServletResponse
+     */
+    @GetMapping("/export")
+    @ApiOperation("导出运营数据报表")
+    public void export(HttpServletResponse httpServletResponse) {
+        reportService.exportData(httpServletResponse);
     }
 }
